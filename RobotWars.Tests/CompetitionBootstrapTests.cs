053faedfc22da0a3029c9ccbe8ce27c292c17e3a
MaksymRybak <rybak.maksym@gmail.com>
2016-06-30 00:55:54 +0200

    added CompetitionBootstrap tests

---
+++ RobotWars.Tests/CompetitionBootstrapTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace RobotWars.Tests
+{
+    [TestFixture]
+    public class CompetitionBootstrapTests
+    {
+        private CompetitionBootstrap competitionBootstrap;
+
+        [SetUp]
+        public void SetUp()
+        {
+            competitionBootstrap = new CompetitionBootstrap(null, null, null, null, null);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            competitionBootstrap = null;
+        }
+
+        [Test]
+        public void Should_bootstrap_competition()
+        {
+            // TODO: test passing not null value
+            competitionBootstrap.Start(null);
+        }
+    }
+}
